30aa16271ef4bd5b68d068724646229f68860346
Atchaya Namachivayam <IS7114@integra.co.in>
2019-11-29 11:08:39 +0530

    Updates

---
+++ precache-manifest.bf6d31c94562686f1fa9f46b6ebed4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea56c9fa7939365c701",
+    "url": "/AtchayaNamachivayam/deploy_quiz/static/css/main.cd3f2c21.chunk.css"
+  },
+  {
+    "revision": "6ea56c9fa7939365c701",
+    "url": "/AtchayaNamachivayam/deploy_quiz/static/js/main.8f701948.chunk.js"
+  },
+  {
+    "revision": "5fe7184b64230ef68a7c",
+    "url": "/AtchayaNamachivayam/deploy_quiz/static/js/runtime~main.3376dca4.js"
+  },
+  {
+    "revision": "5ad9e9d6fec0ec135cb7",
+    "url": "/AtchayaNamachivayam/deploy_quiz/static/js/2.f6711c6e.chunk.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/AtchayaNamachivayam/deploy_quiz/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "fb3e7458bea6c2b48843b5ba7b950d27",
+    "url": "/AtchayaNamachivayam/deploy_quiz/index.html"
+  }
+];
